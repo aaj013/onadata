8c48124644f63c9de6212aef389e3a07e6d70796
Davis Raymond Muro <davisraymondmuro@outlook.com>
2020-01-31 12:28:31 +0300

    Update enketo url management commands
    - Update create_enketo_express_urls to utilize a forms uuid
      to generate the enketo urls when username and id_string are not
      passed
    - Update update_enketo_urls to utilize a forms uuid to update
      enketo urls when the --use-username flag is False

--- onadata/apps/main/management/commands/update_enketo_urls.py
+++ onadata/apps/main/management/commands/update_enketo_urls.py
@@ -18,12 +18,15 @@ class Command(BaseCommand):
             "-p", "--server_port", dest="server_port", default="443")
         parser.add_argument(
             "-r", "--protocol", dest="protocol", default="https")
+        parser.add_argument(
+            "-u", "--use-username", dest="use_username", default=False)
 
     def handle(self, *args, **options):
         request = HttpRequest()
         server_name = options.get('server_name')
         server_port = options.get('server_port')
         protocol = options.get('protocol')
+        use_username = options.get('use_username')
 
         if not server_name or not server_port or not protocol:
             raise CommandError(
@@ -45,25 +48,45 @@ class Command(BaseCommand):
 
         resultset = MetaData.objects.filter(
             Q(data_type='enketo_url') | Q(data_type='enketo_preview_url'))
-        for meta_data in resultset:
-            username = meta_data.content_object.user.username
-            id_string = meta_data.content_object.id_string
-            data_type = meta_data.data_type
-            data_value = meta_data.data_value
-            xform = meta_data.content_object
-            with open('/tmp/enketo_url', 'a') as f:
 
-                if data_type == 'enketo_url':
-                    form_url = get_form_url(
-                        request, username, protocol=protocol,
-                        xform_pk=xform.pk)
-                    _enketo_url = enketo_url(form_url, id_string)
-                    MetaData.enketo_url(xform, _enketo_url)
-                elif data_type == 'enketo_preview_url':
-                    _enketo_preview_url = get_enketo_preview_url(
-                        request, username, id_string, xform_pk=xform.pk)
-                    MetaData.enketo_preview_url(xform, _enketo_preview_url)
-                f.write('%s : %s \n' % (id_string, data_value))
+        if use_username:
+            for meta_data in resultset:
+                username = meta_data.content_object.user.username
+                id_string = meta_data.content_object.id_string
+                data_type = meta_data.data_type
+                data_value = meta_data.data_value
+                xform = meta_data.content_object
+                with open('/tmp/enketo_url', 'a') as f:
+
+                    if data_type == 'enketo_url':
+                        form_url = get_form_url(
+                            request, username, protocol=protocol,
+                            xform_pk=xform.pk)
+                        _enketo_url = enketo_url(form_url, id_string)
+                        MetaData.enketo_url(xform, _enketo_url)
+                    elif data_type == 'enketo_preview_url':
+                        _enketo_preview_url = get_enketo_preview_url(
+                            request, username, id_string, xform_pk=xform.pk)
+                        MetaData.enketo_preview_url(xform, _enketo_preview_url)
+                    f.write('%s : %s \n' % (id_string, data_value))
+        else:
+            for meta_data in resultset:
+                data_type = meta_data.data_type
+                data_value = meta_data.data_value
+                xform = meta_data.content_object
+
+                with open('/tmp/enketo_url', 'a') as f:
+                    if data_type == 'enketo_url':
+                        form_url = get_form_url(
+                            request, protocol=protocol,
+                            xform_uuid=xform.uuid)
+                        _enketo_url = enketo_url(form_url, id_string)
+                        MetaData.enketo_url(xform, _enketo_url)
+                    elif data_type == 'enketo_preview_url':
+                        _enketo_preview_url = get_enketo_preview_url(
+                            request, id_string, xform_uuid=xform.uuid)
+                        MetaData.enketo_preview_url(xform, _enketo_preview_url)
+                    f.write('%s : %s \n' % (id_string, data_value))
 
             self.stdout.write('%s: %s' % (data_type, meta_data.data_value))
 
